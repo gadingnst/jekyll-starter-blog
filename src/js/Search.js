6fa46adca19fe619b7419e5d9bdbe9150a910d43
Sutan Nst <sutan.gnst@gmail.com>
2019-05-01 00:19:31 +0700

    auto committing changes in Rab Mei  1 00:19:31 WIB 2019

--- src/js/Search.js
+++ src/js/Search.js
@@ -7,7 +7,7 @@
       searchResults: '.search-results',
       searchResultsTitle: '<h4>Hasil Pencarian:</h4>',
       limit: '10',
-      noResults: '<p>Oops!<br/><small>Tidak ada hasil ! :(</small></p>'
+      noResults: '<p>Oops!<br/><small>Tidak ada hasil ! 🙁</small></p>'
     }, options)
 
     let properties = settings.jsonFormat.split(',')
